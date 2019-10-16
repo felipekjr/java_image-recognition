34f1151b82e9346fc4eb10b54d6f8e3919cae211
felipekjr <felipekjr@gmail.com>
2019-10-16 14:25:09 -0300

    Iniciar projeto JavaFX no eclipse

---
+++ src/br/imd/Principal.java
@@ -0,0 +1,16 @@
+package br.imd;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Principal extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
