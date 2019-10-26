1fb833eb3508d8884c1e0e29917c38a2a2a30dab
felipekjr <felipekjr@gmail.com>
2019-10-26 00:17:18 -0300

    Criar file chooser

--- src/br/imd/controle/TelaPrincipalController.java
+++ src/br/imd/controle/TelaPrincipalController.java
@@ -1,10 +1,12 @@
 package br.imd.controle;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import br.imd.Principal;
+import br.imd.dao.ParametrosAnaliseDAO;
 import br.imd.modelo.MedidaDistancia;
 import br.imd.modelo.ParametrosAnalise;
 import javafx.collections.FXCollections;
@@ -16,15 +18,23 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class TelaPrincipalController {
-	
+	// classes
 	private Principal main;
 	ParametrosAnalise parametrosAnalise = new ParametrosAnalise();
-	
+	ParametrosAnaliseDAO dao = new ParametrosAnaliseDAO();
+	// elementos da view
 	@FXML private TextField textField;
     @FXML private ComboBox<MedidaDistancia> comboBox = new ComboBox<MedidaDistancia>();
     @FXML private Button button;
+    // variáveis locais
+    private MedidaDistancia medida;
+    private String texto;
+    private String imagem;
+    
     
     public void setMainApp(Principal main) {
 		this.main = main;
@@ -43,15 +53,24 @@ public class TelaPrincipalController {
     protected void initialize() {
     	// escutar campo de texto    	
     	textField.textProperty().addListener((observable, oldValue, newValue) -> {
-    		this.parametrosAnalise.setK(Integer.parseInt(newValue));
+    		this.texto = newValue;
         });   
     	// inicializa o select
     	comboBox.setItems(FXCollections.observableArrayList(MedidaDistancia.EUCLIDIANA, MedidaDistancia.CHEBYCHEV));
-    	comboBox.valueProperty().addListener((newValue) -> System.out.println(newValue));
+    	comboBox.valueProperty().addListener((observable, oldValue, newValue) -> this.medida = newValue);
+    }
+    
+    @FXML
+    private void escolherImagem(ActionEvent event) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Escolher imagem");
+        File file = chooser.showOpenDialog(new Stage());
+        this.imagem = file.getAbsolutePath();
     }
     
     @FXML
     private void handleSubmitAction(ActionEvent event) {
-    	 	
+    	System.out.println(this.texto + ' ' + this.medida + ' ' + this.imagem );
+    	dao.calcular(this.texto, this.medida, this.imagem);
     }
 }
--- src/br/imd/dao/ParametrosAnaliseDAO.java
+++ src/br/imd/dao/ParametrosAnaliseDAO.java
@@ -1,5 +1,7 @@
 package br.imd.dao;
 
 public class ParametrosAnaliseDAO {
-
+	public void calcular() {
+		
+	}
 }
