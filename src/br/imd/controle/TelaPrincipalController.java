49f842c9844fb97c549fc382b8deaed654b1ada8
yagobmarques <yagobsmarques@gmail.com>
2019-11-06 09:42:41 -0300

    verificação na DAO, tirando o retorno do if, explicando melhor o motivo do erro e alteração para showError

--- src/br/imd/controle/TelaPrincipalController.java
+++ src/br/imd/controle/TelaPrincipalController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,112 +33,139 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class TelaPrincipalController {
-	
+
 	private Principal main;
 	ParametrosAnalise parametrosAnalise = new ParametrosAnalise();
-	ParametrosAnaliseDAO dao = new ParametrosAnaliseDAO();	
-	
-	@FXML private Text textoResultado;	
-    @FXML private ComboBox<MedidaDistancia> comboBoxMedida = new ComboBox<MedidaDistancia>();
-    @FXML private ComboBox<Integer> comboBoxK = new ComboBox<Integer>();
-    @FXML private ImageView imageView;    
-    @FXML private Button botaoApagar;
-   
-    private MedidaDistancia medida;
-    private Integer valorK = 0;
-    private String imagem;
-    private boolean hasPerson;
-    
-    
-    public void setMainApp(Principal main) {
+	ParametrosAnaliseDAO dao = new ParametrosAnaliseDAO();
+
+	@FXML
+	private Text textoResultado;
+	@FXML
+	private ComboBox<MedidaDistancia> comboBoxMedida = new ComboBox<MedidaDistancia>();
+	@FXML
+	private ComboBox<Integer> comboBoxK = new ComboBox<Integer>();
+	@FXML
+	private ImageView imageView;
+	@FXML
+	private Button botaoApagar;
+
+	private MedidaDistancia medida;
+	private Integer valorK = 0;
+	private String imagem;
+	private boolean hasPerson;
+
+	public void setMainApp(Principal main) {
 		this.main = main;
-	}    
-    
-    @FXML
-    void abrirTelaPrincipal(ActionEvent event) {
-    	try {
+	}
+
+	@FXML
+	void abrirTelaPrincipal(ActionEvent event) {
+		try {
 			main.iniciarTela();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    }
-    
-    @FXML
-    protected void initialize() {    	
-    	imageView.setVisible(false);
-    	textoResultado.setVisible(false);
-    	botaoApagar.setVisible(false);    	
-    	
-    	comboBoxMedida.setItems(FXCollections.observableArrayList(
-    			MedidaDistancia.EUCLIDIANA, 
-    			MedidaDistancia.CHEBYSHEV,
-    			MedidaDistancia.MANHATTAN
-    			));
+	}
 
-    	comboBoxMedida.valueProperty().addListener((observable, oldValue, newValue) -> this.medida = newValue);    
-    	comboBoxK.setItems(FXCollections.observableArrayList(1,2,3,4,5,6,7,8,9,10));
-    	comboBoxK.valueProperty().addListener((observable, oldValue, newValue) -> this.valorK = newValue); 
-    }
-    
-    @FXML
-    private void escolherImagem(ActionEvent event) throws IOException {
-    	try {
-    		FileChooser chooser = new FileChooser();
-	        chooser.setTitle("Escolher imagem");
-	        File file = chooser.showOpenDialog(new Stage());
-	        imagem = file.getAbsolutePath();        
-	        Image image = new Image(new FileInputStream(this.imagem));       
-	        this.showImage(image);  
-    	} catch (Exception e) {
-    		System.out.println("Erro ao escolher imagem.Por favor, escolha novamente!");
-    	}
-      
-    }
-    
-    @FXML
-    private void apagarImagem(ActionEvent event) throws IOException {
-    	imagem = "";
-    	imageView.setVisible(false);
-    	botaoApagar.setVisible(false);
-    }
-    
-    @FXML
-    private void handleSubmitAction(ActionEvent event) {    	
-    	try {
-    		if (dao.verificar(this.valorK, this.medida, this.imagem)== false){
-    			this.showResult();
-    			return;
-    		}
-    		hasPerson = dao.calcular(this.valorK, this.medida, this.imagem);
-        	this.showResult(hasPerson);
-    	} catch(Exception e) {
-    		System.out.println("Ocorreu um erro de submissão. Por favor, preencha os campos corretamente!");
-    	}
-    	
-    }
-    
-    private void showImage(Image image) {
-    	Rectangle clip = new Rectangle(imageView.getFitWidth(), imageView.getFitHeight());
+	@FXML
+	protected void initialize() {
+		imageView.setVisible(false);
+		textoResultado.setVisible(false);
+		botaoApagar.setVisible(false);
+
+		comboBoxMedida.setItems(FXCollections.observableArrayList(MedidaDistancia.EUCLIDIANA, MedidaDistancia.CHEBYSHEV,
+				MedidaDistancia.MANHATTAN));
+
+		comboBoxMedida.valueProperty().addListener((observable, oldValue, newValue) -> this.medida = newValue);
+		comboBoxK.setItems(FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+		comboBoxK.valueProperty().addListener((observable, oldValue, newValue) -> this.valorK = newValue);
+	}
+
+	@FXML
+	private void escolherImagem(ActionEvent event) throws IOException {
+		try {
+			FileChooser chooser = new FileChooser();
+			chooser.setTitle("Escolher imagem");
+			File file = chooser.showOpenDialog(new Stage());
+			imagem = file.getAbsolutePath();
+			Image image = new Image(new FileInputStream(this.imagem));
+			this.showImage(image);
+		} catch (Exception e) {
+			System.out.println("Erro ao escolher imagem.Por favor, escolha novamente!");
+		}
+
+	}
+
+	@FXML
+	private void apagarImagem(ActionEvent event) throws IOException {
+		imagem = "";
+		imageView.setVisible(false);
+		botaoApagar.setVisible(false);
+	}
+
+	@FXML
+	private void handleSubmitAction(ActionEvent event) {
+		String erro;
+		try {
+			erro = this.verificar(this.valorK, this.medida, this.imagem);
+			if (erro != "") {
+				this.showError(erro);
+			} else {
+				hasPerson = dao.calcular(this.valorK, this.medida, this.imagem);
+				this.showResult(hasPerson);
+			}
+		} catch (Exception e) {
+			System.out.println("Ocorreu um erro de submissão. Por favor, preencha os campos corretamente!");
+		}
+
+	}
+
+	private void showImage(Image image) {
+		Rectangle clip = new Rectangle(imageView.getFitWidth(), imageView.getFitHeight());
 		clip.setArcWidth(70);
 		clip.setArcHeight(70);
-		imageView.setClip(clip);		
+		imageView.setClip(clip);
 		imageView.setImage(image);
 		imageView.setVisible(true);
 		botaoApagar.setVisible(true);
-    }
-    
-    private void showResult() {
-    	textoResultado.setText("Erros de parâmetros!");
-    	textoResultado.getStyleClass().clear();
-    	textoResultado.getStyleClass().add("texto-resultado");
-    	textoResultado.getStyleClass().add("vermelho");
-    	textoResultado.setVisible(true);  
-    }
-    private void showResult(boolean hasPerson) {    	
-    	textoResultado.setText(hasPerson ? "Existe uma pessoa na imagem!" : "Não existe pessoa na imagem!" );
-    	textoResultado.getStyleClass().clear();
-    	textoResultado.getStyleClass().add("texto-resultado");
-    	textoResultado.getStyleClass().add(hasPerson ? "verde" : "vermelho");
-    	textoResultado.setVisible(true);    	
-    }
+	}
+
+	private void showError(String erro) {
+		textoResultado.setText(erro);
+		textoResultado.getStyleClass().clear();
+		textoResultado.getStyleClass().add("texto-resultado");
+		textoResultado.getStyleClass().add("vermelho");
+		textoResultado.setVisible(true);
+	}
+
+	private void showResult(boolean hasPerson) {
+		textoResultado.setText(hasPerson ? "Existe uma pessoa na imagem!" : "Não existe pessoa na imagem!");
+		textoResultado.getStyleClass().clear();
+		textoResultado.getStyleClass().add("texto-resultado");
+		textoResultado.getStyleClass().add(hasPerson ? "verde" : "vermelho");
+		textoResultado.setVisible(true);
+	}
+
+	public final String verificar(int k, MedidaDistancia medidaDistancia, String caminhoImagem) throws Exception {
+		String mimeType = null;
+		if (medidaDistancia == null) {
+			return "Insira uma métrica de distância";
+		}
+		if (k == 0) {
+			return "Insira um valor K";
+		}
+		if (caminhoImagem == null) {
+			return "Insira uma imagem";
+		}
+		try {
+			java.nio.file.Path path = (java.nio.file.Path) new File(caminhoImagem).toPath();
+			mimeType = Files.probeContentType(path);
+		} catch (Exception a) {
+			System.out.println("Erro de abertura da imagem");
+		}
+		if (mimeType.equals("image/png")) {
+			return "";
+		}
+		return "A imagem passada não está no formato PNG";
+	}
 }
--- src/br/imd/dao/ParametrosAnaliseDAO.java
+++ src/br/imd/dao/ParametrosAnaliseDAO.java
@@ -13,49 +13,34 @@ import br.imd.modelo.MedidaDistancia;
 import utils.CsvUtil;
 import utils.ImageUtil;
 
-public class ParametrosAnaliseDAO {	
-	
-		ArrayList<String[]> dataset = getDataSet();
-		public boolean verificar(int k, MedidaDistancia medidaDistancia, String caminhoImagem) throws Exception{
-			String mimeType = null;
-			if (k == 0 || medidaDistancia == null || caminhoImagem == null) {
-				return false;
-			}
-			try {
-				java.nio.file.Path path = (java.nio.file.Path) new File(caminhoImagem).toPath();
-				 mimeType = Files.probeContentType(path);
-			} catch (Exception a){
-				System.out.println("Erro de abertura da imagem");
-			}
-			if (mimeType.equals("image/png")) {
-				return true;
-			}
-			return false;
-		}
-		public boolean calcular(int k, MedidaDistancia medidaDistancia, String caminhoImagem) throws Exception {
-		ArrayList<Float> hogAtributes = ImageUtil.extractAttr(caminhoImagem);		
+public class ParametrosAnaliseDAO {
+
+	ArrayList<String[]> dataset = getDataSet();
+
+	public boolean calcular(int k, MedidaDistancia medidaDistancia, String caminhoImagem) throws Exception {
+		ArrayList<Float> hogAtributes = ImageUtil.extractAttr(caminhoImagem);
 		Dictionary<Integer, Float> distancias;
-		
+
 		switch (medidaDistancia) {
-			case EUCLIDIANA:
-				distancias = Euclidiana.calcular(hogAtributes, dataset);
-				System.out.println(Knn.calcular(dataset, distancias, k));
-				return Knn.calcular(dataset, distancias, k);				
-			case CHEBYSHEV:
-				distancias = Chebyshev.calcular(hogAtributes, dataset);
-				System.out.println(Knn.calcular(dataset, distancias, k));
-				return Knn.calcular(dataset, distancias, k);					
-			case MANHATTAN:			
-				distancias = Manhattan.calcular(hogAtributes, dataset);
-				System.out.println(Knn.calcular(dataset, distancias, k));
-				return Knn.calcular(dataset, distancias, k);		
+		case EUCLIDIANA:
+			distancias = Euclidiana.calcular(hogAtributes, dataset);
+			System.out.println(Knn.calcular(dataset, distancias, k));
+			return Knn.calcular(dataset, distancias, k);
+		case CHEBYSHEV:
+			distancias = Chebyshev.calcular(hogAtributes, dataset);
+			System.out.println(Knn.calcular(dataset, distancias, k));
+			return Knn.calcular(dataset, distancias, k);
+		case MANHATTAN:
+			distancias = Manhattan.calcular(hogAtributes, dataset);
+			System.out.println(Knn.calcular(dataset, distancias, k));
+			return Knn.calcular(dataset, distancias, k);
 		}
-		
+
 		return false;
 	}
-	
-	private ArrayList<String[]> getDataSet(){
+
+	private ArrayList<String[]> getDataSet() {
 		return new CsvUtil().extractDataset("src/resources/datasets/attr_imagens.csv");
 	}
-	
+
 }
